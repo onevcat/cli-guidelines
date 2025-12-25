39cd8e9d8d4c1a5aa5b40e922574a172f6897c7b
onevcat <wei.wang@linecorp.com>
2025-12-25 17:05:48 +0900

    Better CJK script

--- scripts/fix-cjk-spacing.sh
+++ scripts/fix-cjk-spacing.sh
@@ -2,6 +2,6 @@
 # Post-process HTML to remove newlines/spaces after Chinese punctuation
 # Usage: Run after `hugo` build
 
-find public -name "*.html" -exec perl -i -p0e 's/([，。、；：""''（）！？》])[\n ]+/\1/g' {} \;
+find public -name "*.html" -exec perl -i -p0e 's/([，。、；：""''（）！？》])\n+/\1/g' {} \;
 
 echo "CJK spacing fixed in public/"
